a6c8b65b0f5108e83715757b30575d91efb0482b
GGLVXD <gglvmax@gmail.com>
2025-04-23 21:27:50 +0300

    added paths for each file, for better navigation

--- src/lib/components/Header/Header.svelte
+++ src/lib/components/Header/Header.svelte
@@ -1,3 +1,4 @@
+<!--/src/lib/components/Header/Header.svelte-->
 <script>
   import { onMount } from 'svelte';
   import banner from '$lib/assets/banner.png';
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,4 @@
+<!--/src/routes/+page.svelte-->
 <script>
     import Header from '$lib/components/Header/Header.svelte'
     import '$lib/components/Global/Background.css';
