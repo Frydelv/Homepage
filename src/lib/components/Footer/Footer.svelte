6e7852f635ffdebe9588be7ed6a7ef57337562fc
GGLVXD <gglvmax@gmail.com>
2025-04-23 21:53:41 +0300

    half baked footer

---
+++ src/lib/components/Footer/Footer.svelte
@@ -0,0 +1,12 @@
+<!--/src/lib/components/Footer/Footer.svelte-->
+<style>
+    footer {
+        text-align: center;
+        padding: 3px;
+        margin: 0;
+        bottom: -50px;
+    }
+</style>
+<footer>
+    <h1>hey</h1>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <!--/src/routes/+page.svelte-->
 <script>
     import Header from '$lib/components/Header/Header.svelte'
+    import Footer from '$lib/components/Footer/Footer.svelte'
     import '$lib/components/Global/Background.css';
 </script>
 
 <Header />
+<Footer />
 
 <body>
 </body>
